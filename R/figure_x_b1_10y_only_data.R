b643d7cb42bb3d30222105e3b27ee0da36cdd88e
Mahood <12001002933729@FEDIDCARD.GOV>
2025-02-27 15:54:11 -0700

    j

--- R/figure_x_b1_10y_only_data.R
+++ R/figure_x_b1_10y_only_data.R
@@ -12,8 +12,9 @@ dl$kf <- khr_fuels |>
     loading_t_ac = volume_ft3 * density_constant * area.multiplier * 10) |> 
   group_by(new_visit_code, phase_adj, PlotTreatmentStatus, TreatmentUnit) |>
   reframe(loading_t_ac = sum(loading_t_ac, na.rm=T)) |>
-  dplyr::select(new_visit_code, PlotTreatmentStatus, value = loading_t_ac) |>
-  dplyr::mutate(name = "Khr Fuels (t/ac)")
+  dplyr::select(new_visit_code, PlotTreatmentStatus, value = loading_t_ac)  |>
+  dplyr::mutate(value = value*2.52)|>
+  dplyr::mutate(name = "Khr Fuels (t/ha)")
 
 # dl$kf |>
 #   ggplot(aes(x=new_visit_code |> str_sub(1,1), y=value, fill=PlotTreatmentStatus)) +
@@ -26,7 +27,9 @@ dl$saps <- cp_sapling |>
   ungroup() |>
   filter(phase_adj %in% c('post10-11')) |>
   pivot_longer(cols = c(saplings_per_acre)) |>
-  dplyr::select(-phase_adj)
+  dplyr::select(-phase_adj) |>
+  dplyr::mutate(name = 'Saplings/ha', 
+                value  = value * 2.47)
 
 # dl$saps |>
 #   ggplot(aes(x=new_visit_code |> str_sub(1,1), y=value, fill = PlotTreatmentStatus)) +
@@ -34,9 +37,9 @@ dl$saps <- cp_sapling |>
 #   facet_wrap(~name, scales = "free_y") +
 #   ggtitle("Saplings")
 
-dl$seeds <- seedling_density |>
-  dplyr::rename(value = seedlings_per_acre) |>
-  mutate(name = 'seedlings_per_acre')
+# dl$seeds <- seedling_density |>
+#   dplyr::rename(value = seedlings_per_acre) |>
+#   mutate(name = 'seedlings_per_acre')
 
 # Convert Litter & Duff depth to Loading - CHANGE MULTIPLIER BASED ON FOREST TYPE (BELOW)
 # PIPO- litter=2.37 tons/ac/in (Ziegler 2014 thesis), duff= 7.27 (Battaglia et al 2010)
@@ -56,7 +59,8 @@ dl$gf <- sp_groundfuel |>
   group_by(new_visit_code,PlotTreatmentStatus) |>
   summarise(value = sum(loading)) |>
   ungroup() |>
-  mutate(name = 'litter/duff fuel load (t/ac)')
+  dplyr::mutate(value = value*2.52) |>
+  mutate(name = 'Litter/Duff Fuel Load (t/ha)')
 
 dl$wf <- sp_woodyfuel |>
   dplyr::select(new_visit_code, Measurement, load = Calibrated,PlotTreatmentStatus) |>
@@ -64,21 +68,23 @@ dl$wf <- sp_woodyfuel |>
   summarise(load= sum(load)) |>
   ungroup() |>
   dplyr::rename(value = load) |>
-  dplyr::mutate(name = '1/10/100 hour fuels (t/ac)')
+  dplyr::mutate(value = value*2.52,
+    name = '1/10/100 Hour Fuels (t/ha)')
 
 dl$tcc <- tree_canopy_cover |>
   dplyr::filter(phase_adj == 'post10-11') |>
   group_by(new_visit_code,PlotTreatmentStatus) |>
   summarise(value = sum(PercentCover)) |>
   ungroup() |>
-  dplyr::mutate(name = 'tree_canopy_cover')
+  dplyr::mutate(name = 'Tree Canopy Cover (%)')
 
 dl$sh <- understory_heights |>
   filter(!is.na(Height_inches)) |>
   group_by(new_visit_code,PlotTreatmentStatus) |>
   summarise(value = mean(Height_inches)) |>
   ungroup() |>
-  mutate(name = 'shrub_height_in')
+  mutate(value = value * 2.54) |>
+  mutate(name = 'Shrub Height (cm)')
 
 
 
@@ -110,24 +116,29 @@ stars <- bind_rows(anovas) |>
   dplyr::select(name, term, p.value) |>
   mutate(term = ifelse(term == "site", "Site", "Treatment")) |>
   filter(term == "Treatment") |>
-  mutate(sig = ifelse(p.value < 0.05, '*', "")) |>
-  dplyr::select(name, sig) |>
-  mutate(position = c(15, 2000, 2000, 15, 12, 70, 15))
+  dplyr::mutate(star = case_when(p.value >= 0.1 ~ "",
+                                 p.value < 0.1 & p.value >= 0.05 ~ '.',
+                                 p.value < 0.05 & p.value >= 0.01 ~ '*',
+                                 p.value < 0.01 & p.value >= 0.001 ~ '**',
+                                 p.value < 0.001 ~ "***"))  |>
+  dplyr::select(name, star) |>
+  mutate(position = c(45, 5000,  45, 30, 70, 15))
 
 p <- bind_rows(dl) |>
   left_join(stars) |>
   mutate(site = ifelse(str_sub(new_visit_code, 1,1) == "E", "Estes Valley", "Phantom Creek")) |>
   ggplot(aes(x=PlotTreatmentStatus, y=value, fill = PlotTreatmentStatus)) +
   geom_boxplot(outliers =F) +
-  geom_text(aes(label = sig, y=position), x=1.5, size=12) +
+  geom_text(aes(label = star, y=position), x=1.5, size=8) +
   facet_wrap(~name, scales ='free') +
   xlab("Site") +
   scale_fill_brewer(palette = "Set1") +
+  scale_fill_manual(values = c('#FF00C5', '#00A884')) +
   ggtitle("Ten Years Post-Treatment") +
   theme_bw() +
-  theme(legend.position = c(.9,0.1),
-        legend.justification = c(1,0),
+  theme(#legend.position = c(.9,0.1),
+        #legend.justification = c(1,0),
         legend.title = element_blank(),
         axis.title.y = element_blank()) ;p
 
-ggsave(p, filename = 'out/ten_year_only_variables.png', width = 8, height = 6)
+ggsave(p, filename = 'out/ten_year_only_variables.png', width = 7, height = 4.5)
